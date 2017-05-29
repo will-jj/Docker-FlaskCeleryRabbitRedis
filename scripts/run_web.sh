f14cbcfc71b13e92ff17db8ce3e6cbdc288e5083
William Jackson <william.jackson@strath.ac.uk>
2017-05-29 17:59:14 +0100

    wsgi

--- scripts/run_web.sh
+++ scripts/run_web.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 cd strava_flask
-su -m app -c "python app.py"
+#su -m app -c "python app.py"
+#su -m app -c "uwsgi --socket 0.0.0.0:8000 --protocol=http -w wsgi"
+su -m app -c "uwsgi --ini app.ini"
